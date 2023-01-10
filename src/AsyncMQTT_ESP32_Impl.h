4a9f8bcdb04e6c0c329bda40896afd4d7d8e31c7
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2023-01-09 19:50:34 -0500

    v1.10.0 for `LwIP W6100`
    ### Releases v1.10.0
    
    1. Add support to `ESP32` and `ESP32S2/S3/C3` boards using `LwIP W6100 Ethernet`

--- src/AsyncMQTT_ESP32_Impl.h
+++ src/AsyncMQTT_ESP32_Impl.h
@@ -1,21 +1,22 @@
 /****************************************************************************************************************************
   AsyncMQTT_ESP32_Impl.h
 
-  AsyncMQTT_ESP32 is a library for ESP32 boards using WiFi or LwIP W5500, LAN8720 or ENC28J60
+  AsyncMQTT_ESP32 is a library for ESP32 boards using WiFi or LwIP W5500 / W6100 / ENC28J60 / LAN8720 Ethernet
 
   Based on and modified from :
 
   1) async-mqtt-client (https://github.com/marvinroger/async-mqtt-client)
-  2) async-mqtt-client (https://github.com/khoih-prog/AsyncMQTT_Generic)
+  2) AsyncMQTT_Generic (https://github.com/khoih-prog/AsyncMQTT_Generic)
 
   Built by Khoi Hoang https://github.com/khoih-prog/AsyncMQTT_ESP32
 
-  Version: 1.9.0
+  Version: 1.10.0
 
   Version Modified By   Date      Comments
   ------- -----------  ---------- -----------
   1.8.0    K Hoang     17/12/2022 Initial coding to port to ESP32 boards using WiFi or LwIP W5500, LAN8720 or ENC28J60
   1.9.0    K Hoang     21/12/2022 Add support to ESP32S2/C3 boards using LwIP W5500 or ENC28J60 Ethernet
+  1.10.0   K Hoang     09/01/2023 Add support to ESP32 and ESP32S2/S3/C3 boards using LwIP W6100
  *****************************************************************************************************************************/
 
 #pragma once
--- src/AsyncMqttClient/Packets/Out/Subscribe.hpp
+++ src/AsyncMqttClient/Packets/Out/Subscribe.hpp
@@ -1,13 +1,14 @@
 /****************************************************************************************************************************
   Subscribe.hpp
   
-  AsyncMqttClient_Generic is a library for ESP32, ESP8266, Protenta_H7, STM32F7, etc. with current AsyncTCP support
-  
+  AsyncMQTT_ESP32 is a library for ESP32 boards using WiFi or LwIP W5500 / W6100 / ENC28J60 / LAN8720 Ethernet
+
   Based on and modified from :
-  
+
   1) async-mqtt-client (https://github.com/marvinroger/async-mqtt-client)
-  
-  Built by Khoi Hoang https://github.com/khoih-prog/AsyncMqttClient_Generic
+  2) AsyncMQTT_Generic (https://github.com/khoih-prog/AsyncMQTT_Generic)
+
+  Built by Khoi Hoang https://github.com/khoih-prog/AsyncMQTT_ESP32
  *****************************************************************************************************************************/
 
 #pragma once
